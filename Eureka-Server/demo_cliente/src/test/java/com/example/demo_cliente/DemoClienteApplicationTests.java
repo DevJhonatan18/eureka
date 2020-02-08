07db883749f2900bc41fe832476f7bd80b2e3d6c
Jhonatan Urbina <jhonatan.urbina@unmsm.edu.pe>
2020-02-08 13:59:18 -0500

    first commit

---
+++ Eureka-Server/demo_cliente/src/test/java/com/example/demo_cliente/DemoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
